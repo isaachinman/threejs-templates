087abb58e5edcaa03adc89fe4ad56c19972ad132
isaachinman <isaacshinman@gmail.com>
2016-06-17 10:03:32 +0200

    Using a more complex city model

--- building-scene/building-scene.js
+++ building-scene/building-scene.js
@@ -1,62 +1,59 @@
-let D, aspect, camera, light, loader, renderer, scene
-
 let width = window.innerWidth
 let height = window.innerHeight
+let scene = new THREE.Scene()
+let camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
 
-aspect = width / height
-
-D = 8
-
-scene = new THREE.Scene()
-
-camera = new THREE.OrthographicCamera(-D * aspect, D * aspect, D, -D, 1, 1000)
-
-renderer = new THREE.WebGLRenderer({
+let renderer = new THREE.WebGLRenderer({
     alpha: true,
     antialias: true
 })
 
+// Set renderer size and append it
 renderer.setSize(width, height)
-
 document.body.appendChild(renderer.domElement)
 
-// Create a grass texture
-let texture = THREE.ImageUtils.loadTexture('grass.jpg')
-texture.wrapS = THREE.RepeatWrapping
-texture.wrapT = THREE.RepeatWrapping
-texture.repeat.set(100, 100)
-
 // Make some grass
-let grassMaterial = new THREE.MeshLambertMaterial({ map: texture })
-let plane = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000), grassMaterial)
-plane.position.x = 0
-plane.rotation.z = Math.PI / 2
-scene.add(plane)
+let textureLoader = new THREE.TextureLoader()
+textureLoader.load('grass.jpg', function(texture) {
 
+  // Set texture to tile
+  texture.wrapS = THREE.RepeatWrapping
+  texture.wrapT = THREE.RepeatWrapping
+  texture.repeat.set(100, 100)
+
+  let grassMaterial = new THREE.MeshLambertMaterial({ map: texture })
+  let plane = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000), grassMaterial)
+  plane.material.side = THREE.DoubleSide
+  plane.position.x = 0
+  plane.rotation.x = Math.PI / 2
+  plane.position.y = 143
+  scene.add(plane)
+})
 
 // Add a light
-light = new THREE.DirectionalLight(0xffffff, 1)
+let light = new THREE.DirectionalLight(0xeeeeee, 1)
 light.position.set(10, -20, 15)
 scene.add(light)
 
 // Set camera position
-camera.position.set(0, -20, 5)
+camera.position.set(300, 300, 300)
+camera.lookAt(scene.position)
 
 // Add controls
 let controls = new THREE.OrbitControls(camera, renderer.domElement)
 
 // Define the actual render function
 function render(collada) {
-    requestAnimationFrame(render)
-    renderer.render(scene, camera)
+  requestAnimationFrame(render)
+  renderer.render(scene, camera)
 }
 
-loader = new THREE.ColladaLoader()
 
-let lastTimestamp = 0
-
-loader.load('CNR_master.dae', function(collada) {
-    collada.scene.scale.set(0.001, 0.001, 0.001)
+// Model from clara.io/view/6aba987e-344d-47f0-9182-db3898efa6e2#
+let loader = new THREE.ColladaLoader()
+loader.load('city.dae', function(collada) {
+    collada.scene.scale.set(1, 1.5, 1)
+    collada.scene.position.y = -78
     scene.add(collada.scene)
     return render(collada)
 })
