2ba425012cd9e6a207533d96ad2c87199fb2792a
isaachinman <isaacshinman@gmail.com>
2016-06-18 13:58:49 +0200

    Adding sky, grass, and other refinements

--- building-scale-control/building-scale-control.js
+++ building-scale-control/building-scale-control.js
@@ -1,24 +1,42 @@
 let width = window.innerWidth
 let height = window.innerHeight
 let scene = new THREE.Scene()
-let camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
+let camera = new THREE.PerspectiveCamera(80, width / height, 1, 10000 )
 
 let renderer = new THREE.WebGLRenderer({
   alpha: true,
   antialias: true
 })
 
+var ambientLight = new THREE.AmbientLight( 0x000000 ); // soft white light
+scene.add( ambientLight );
+
 // Set renderer size and append it
 renderer.setSize(width, height)
 document.body.appendChild(renderer.domElement)
 
 // Add a light
-let light = new THREE.DirectionalLight(0xeeeeee, 1)
+let light = new THREE.DirectionalLight(0xffffff, 1)
 light.position.set(800, 1200, 500)
 scene.add(light)
 
+// Add some grass
+let grassMaterial = new THREE.MeshLambertMaterial({ color: 0x49E20E })
+let plane = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000), grassMaterial)
+plane.material.side = THREE.DoubleSide
+plane.position.x = 0
+plane.rotation.x = Math.PI / 2
+plane.position.y = -200
+scene.add(plane)
+
+// Make some sky
+let skyMaterial = new THREE.MeshBasicMaterial({ color: 0x87CEFA, side: THREE.BackSide})
+let skyGeometry = new THREE.BoxGeometry(2000,2000,2000)
+let sky = new THREE.Mesh(skyGeometry, skyMaterial)
+scene.add(sky)
+
 // Set camera position
-camera.position.set(800, 1200, 500)
+camera.position.set(420, 200, 200)
 camera.lookAt(scene.position)
 
 // Add controls
@@ -30,8 +48,8 @@ let _collada = null
 
 // Define GUI controls
 let guiControls = new function() {
-  this.height = 0.1
-  this.size = 0.1
+  this.height = 0.05
+  this.size = 0.05
 }
 let datGUI = new dat.GUI()
 datGUI.add(guiControls, 'height', 0, .2)
@@ -48,10 +66,12 @@ function render() {
 
 // Model from clara.io/view/03d6cd28-87e6-4e90-84d8-311c2ced540e#
 let loader = new THREE.ColladaLoader()
+loader.setPreferredShading(THREE.SmoothShading)
 loader.load('building.dae', function(collada) {
-  _collada = collada
-  collada.scene.scale.set(.1, .1, .1)
+
+  collada.scene.scale.set(.05, .05, .05)
   collada.scene.position.y = -205
   scene.add(collada.scene)
+  _collada = collada
   return render(collada)
 })
